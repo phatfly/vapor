a141bc2d1ac04be98752d2b5e070038dddf87591
Christopher Scott <chris@phatfly.com>
2023-12-02 12:36:21 -0600

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -37,7 +37,7 @@ let package = Package(
         .package(url: "https://github.com/apple/swift-nio.git", from: "2.62.0"),
 
         // Bindings to OpenSSL-compatible libraries for TLS support in SwiftNIO
-        .package(url: "https://github.com/apple/swift-nio-ssl.git", branch: "maion"),
+        .package(url: "https://github.com/apple/swift-nio-ssl.git", branch: "main"),
 
         // HTTP/2 support for SwiftNIO
         .package(url: "https://github.com/apple/swift-nio-http2.git", from: "1.28.0"),
